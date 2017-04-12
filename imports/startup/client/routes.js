520e5c048b6a1f10f5df2a84f8410c64e9017862
traina@lungvang.vn <traina@lungvang.vn>
2017-04-12 22:44:30 +0700

    - project

---
+++ imports/startup/client/routes.js
@@ -0,0 +1,24 @@
+import { Router } from 'meteor/iron:router';
+import { Meteor } from 'meteor/meteor';
+
+import '../../ui/layouts/layout.js';
+import '../../ui/pages/homePage/homePage.js';
+import '../../ui/components/404/404.html';
+
+Router.configure({
+  layoutTemplate: "layout",
+  loadingTemplate: 'loadingTemplate',
+  notFoundTemplate: "notFoundTemplate",
+  trackPageView: true,
+  onBeforeAction: function () {
+    this.next();
+  }
+});
+
+Router.route('/', {
+  template: 'homePage',
+  name: 'home',
+  onRun: function () {
+    this.next();
+  }
+});
